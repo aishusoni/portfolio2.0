3df15bb58df9e954a6fe0604a815253c029ec98e
aishusoni <aishujsoni@gmail.com>
2021-05-29 00:57:00 +0530

    deploying

--- src/portfolio2/feedback.js
+++ src/portfolio2/feedback.js
@@ -60,6 +60,42 @@ const Feed=({navopn})=>{
                     );
                 })}                
             </table>
+            <footer style={{
+                marginTop:"17rem",
+                bottom:"0px"
+            }}>
+          <div >
+            {
+              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
+                return(
+                  <div>
+                  <p style={{
+                    textAlign:"center",
+                    color:"white",
+                  }}>
+                    &nbsp;&nbsp;
+                    <b>E-Mail: </b>&nbsp;
+                    {resumeData.email}
+                    &nbsp;&nbsp;
+                    <b>Phone No.</b> : &nbsp;
+                    {resumeData.phone}
+                    &nbsp;&nbsp;
+                    <b>Address</b> : &nbsp;
+                    {resumeData.address}
+                  </p>
+                  <p style={{
+                    textAlign:"center"
+                  }}>
+                    <a href={item.url}>linkedin</a>
+                  </p>
+                  </div>
+                )
+              })
+            }
+          </div>
+          
+
+    </footer>
         </div>
     );
 }
